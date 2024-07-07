edf72d095976842f2c34dd270d47fb2f792843e7
favour-dev <f.ebebeinwe@gmail.com>
2024-07-08 00:59:09 +0100

    Hero Secton

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
 import React from 'react';
 // import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Header from './components/Header';
-import Search from './components/Search';
-// import HomePage from './components/HomePage';
+import Footer from './components/Footer';
+import Hero from './components/Hero';
+// import Partner from './components/Partner'
 // import ProductPage from './components/ProductPage';
 // import CartPage from './components/CartPage';
 
 function App() {
   return(
-    <div className= "App">
-      <Header/>
-      <Search/>
-    </div>
+      <div className= "App">
+        {/* <switch> */}
+          <Header/>
+          <Hero/>
+          
+          <Footer/>
+        {/* </switch> */}
+      </div>
   );
 }
-
 export default App;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,8 @@
 import React from 'react';
-import '../styles/Header.css'; 
+import '../styles/header.css'; 
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
-import { faShoppingCart, faCheck, faUser, faHamburger  } from '@fortawesome/free-solid-svg-icons';
+import { faShoppingCart, faCheck, faUser, faCaretDown, faSearch, faPhone, faBars  } from '@fortawesome/free-solid-svg-icons';
 import HealthXcale from '../assets/images/HealthXcale.png';
-
 
 
 const Header = () => {
@@ -12,7 +11,7 @@ const Header = () => {
             <nav>
                 <div className='nav-container'>
                     <div className='logo'>
-                        <img alt='Logo' src={HealthXcale} />
+                        <img className='logo-img' alt='Logo' src={HealthXcale} />
                     </div>
                     <div className='tag-line'>
                         <p>Nigeria's No.1 Pharmaceutical e-commerce platform</p>
@@ -31,10 +30,28 @@ const Header = () => {
                             <button className='shopmeds-button'>Shop Meds</button>
                         </div>
                         <div className='hamburger-section'>
-                            <FontAwesomeIcon icon= {faHamburger} className='hamburger' />
+                            <FontAwesomeIcon icon= {faBars} className='hamburger' />
                         </div>
                     </div>
                 </div> 
+                <div className='search-container'>
+                    <div className='shop-by'> 
+                        <p className='shop-by-category'>Shop by Category   <span><FontAwesomeIcon icon= {faCaretDown}/></span></p>
+                    </div>
+                    <div className='tag-line2'>
+                        <p>Nigeria's No.1 Pharmaceutical e-commerce platform</p>
+                    </div>
+                    <div className='search-bar'>
+                        <form > 
+                            <span><FontAwesomeIcon icon={faSearch} className="search-icon" /></span>
+                            <input className='search-input' type="text" placeholder="           What Do You Need?" />
+                        </form>
+                    </div>
+                    <div className='call-container'>
+                        <div><FontAwesomeIcon icon= {faPhone} className='phone-icon'/></div>
+                        <div><p className='shop-by-category'>Call - 08127845670</p></div>
+                    </div>
+                </div>
             </nav>
         </header>
     )
